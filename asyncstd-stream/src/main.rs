7394385c8d2e96cfbaa517c85e18b81190648d60
Sebastian Kriems <bastoberto@gmx.de>
2022-09-28 00:37:54 +0200

    async-std: parse message to struct and improved logging

--- asyncstd-stream/src/main.rs
+++ asyncstd-stream/src/main.rs
@@ -1,6 +1,5 @@
 use std::future::Future;
 use std::pin::Pin;
-use std::process;
 use std::time::Duration;
 
 use clap::{App, Arg};
@@ -8,15 +7,23 @@ use futures::stream::StreamExt;
 
 use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
 use rdkafka::consumer::stream_consumer::StreamConsumer;
-use rdkafka::consumer::Consumer;
+use rdkafka::consumer::{CommitMode, Consumer};
 use rdkafka::message::Message;
-use rdkafka::producer::{FutureProducer, FutureRecord};
 use rdkafka::util::AsyncRuntime;
 
-use common::utils::setup_logger;
+use serde::{Deserialize, Serialize};
+
 use common::context::CustomContext;
+use common::utils::setup_logger;
+use log::{error, info, warn};
 
 /// https://github.com/fede1024/rust-rdkafka/blob/master/examples/runtime_async_std.rs
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Payload<'a> {
+    key: &'a str,
+    value: &'a str,
+}
 
 pub struct AsyncStdRuntime;
 
@@ -35,11 +42,79 @@ impl AsyncRuntime for AsyncStdRuntime {
     }
 }
 
+async fn consume(brokers: &str, group_id: &str, topic: &str) {
+    // let producer_context = CustomContext;
+    // let producer: FutureProducer<CustomContext, AsyncStdRuntime> = ClientConfig::new()
+    //     .set("bootstrap.servers", brokers)
+    //     .set("message.timeout.ms", "5000")
+    //     .create_with_context(producer_context)
+    //     .expect("Producer creation error");
+
+    // let delivery_status = producer
+    //     .send::<Vec<u8>, _, _>(
+    //         FutureRecord::to(&topic).payload("hello from async-std"),
+    //         Duration::from_secs(0),
+    //     )
+    //     .await;
+
+    // if let Err((e, _)) = delivery_status {
+    //     eprintln!("unable to send message: {}", e);
+    //     process::exit(1);
+    // }
+
+    let consumer_context = CustomContext;
+    let consumer: StreamConsumer<CustomContext, AsyncStdRuntime> = ClientConfig::new()
+        .set("bootstrap.servers", brokers)
+        .set("session.timeout.ms", "6000")
+        .set("enable.auto.commit", "false")
+        .set("auto.offset.reset", "earliest")
+        .set("group.id", group_id)
+        .set_log_level(RDKafkaLogLevel::Debug)
+        .create_with_context(consumer_context)
+        .expect("Consumer creation failed");
+    consumer.subscribe(&[&topic]).unwrap();
+
+    let mut stream = consumer.stream();
+
+    loop {
+        match stream.next().await {
+            Some(Ok(message)) => match message.payload_view::<str>() {
+                Some(Ok(string)) => match serde_json::from_str::<Payload>(string) {
+                    Ok(parsed) => {
+                        info!(
+                        "Processing message {}, key: {:?}, topic: {}, partition: {}, {:?}: key: {}, value: {}",
+                        message.offset(),
+                        message.key(),
+                        message.topic(),
+                        message.partition(),
+                        message.timestamp(),
+                        parsed.key, parsed.value
+                    );
+                        consumer.commit_message(&message, CommitMode::Async).unwrap();
+                    }
+                    Err(parse_error) => {
+                        error!("Cannot parse message {:?}: {:?}", string, parse_error);
+                        consumer.commit_message(&message, CommitMode::Async).unwrap();
+                    }
+                },
+                Some(Err(_)) => error!("Message is not utf-8 encoded"),
+                None => warn!("Got message without payload"),
+            },
+            Some(Err(e)) => {
+                error!("Error receiving message: {}", e);
+            }
+            None => {
+                warn!("Consumer unexpectedly returned no messages");
+            }
+        }
+    }
+}
+
 #[async_std::main]
 async fn main() {
     let matches = App::new("async-std runtime example")
         .version(option_env!("CARGO_PKG_VERSION").unwrap_or(""))
-        .about("Demonstrates using rust-rdkafka with a custom async runtime")
+        .about("Demonstrates using rust-rdkafka with async-std")
         .arg(
             Arg::with_name("brokers")
                 .short('b')
@@ -77,57 +152,5 @@ async fn main() {
     let topic = matches.value_of("topic").unwrap().to_owned();
     let group_id = matches.value_of("group-id").unwrap();
 
-    let producer_context = CustomContext;
-    let producer: FutureProducer<CustomContext, AsyncStdRuntime> = ClientConfig::new()
-        .set("bootstrap.servers", brokers)
-        .set("message.timeout.ms", "5000")
-        .create_with_context(producer_context)
-        .expect("Producer creation error");
-
-    let delivery_status = producer
-        .send::<Vec<u8>, _, _>(
-            FutureRecord::to(&topic).payload("hello from async-std"),
-            Duration::from_secs(0),
-        )
-        .await;
-
-    if let Err((e, _)) = delivery_status {
-        eprintln!("unable to send message: {}", e);
-        process::exit(1);
-    }
-
-    let consumer_context = CustomContext;
-    let consumer: StreamConsumer<CustomContext, AsyncStdRuntime> = ClientConfig::new()
-        .set("bootstrap.servers", brokers)
-        .set("session.timeout.ms", "6000")
-        .set("enable.auto.commit", "false")
-        .set("auto.offset.reset", "earliest")
-        .set("group.id", group_id)
-        .set_log_level(RDKafkaLogLevel::Debug)
-        .create_with_context(consumer_context)
-        .expect("Consumer creation failed");
-    consumer.subscribe(&[&topic]).unwrap();
-
-    let mut stream = consumer.stream();
-
-    loop {
-        match stream.next().await {
-            Some(Ok(message)) => println!(
-                "Received message: {}",
-                match message.payload_view::<str>() {
-                    None => "",
-                    Some(Ok(s)) => s,
-                    Some(Err(_)) => "<invalid utf-8>",
-                }
-            ),
-            Some(Err(e)) => {
-                eprintln!("Error receiving message: {}", e);
-                process::exit(1);
-            }
-            None => {
-                eprintln!("Consumer unexpectedly returned no messages");
-                process::exit(1);
-            }
-        }
-    }
+    consume(brokers, group_id, &topic).await;
 }
