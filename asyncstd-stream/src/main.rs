0a1fe1467e9d7d09483f8e58e67562020252760d
Sebastian Kriems <bastoberto@gmx.de>
2022-09-30 10:46:03 +0200

    async-std: moved cli into its own module

--- asyncstd-stream/src/main.rs
+++ asyncstd-stream/src/main.rs
@@ -1,6 +1,5 @@
 use std::time::Duration;
 
-use clap::{App, Arg};
 use futures::stream::StreamExt;
 
 use rdkafka::consumer::{CommitMode, Consumer};
@@ -12,6 +11,8 @@ use serde::{Deserialize, Serialize};
 use common::async_std::{create_consumer, create_producer};
 use common::utils::setup_logger;
 use log::{error, info, warn};
+
+mod cli;
 
 /// https://github.com/fede1024/rust-rdkafka/blob/master/examples/runtime_async_std.rs
 
@@ -108,48 +109,7 @@ async fn run(brokers: &str, group_id: &str, input_topic: &str, output_topic: &st
 
 #[async_std::main]
 async fn main() {
-    let matches = App::new("async-std runtime example")
-        .version(option_env!("CARGO_PKG_VERSION").unwrap_or(""))
-        .about("Demonstrates using rust-rdkafka with async-std")
-        .arg(
-            Arg::with_name("brokers")
-                .help("Broker list in kafka format")
-                .short('b')
-                .long("brokers")
-                .takes_value(true)
-                .default_value("localhost:9092"),
-        )
-        .arg(
-            Arg::with_name("group-id")
-                .help("Consumer group id")
-                .short('g')
-                .long("group-id")
-                .takes_value(true)
-                .default_value("async-std"),
-        )
-        .arg(
-            Arg::with_name("input-topic")
-                .help("topic to consume from")
-                .long("input-topic")
-                .short('i')
-                .takes_value(true)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("output-topic")
-                .help("topic to send events to")
-                .long("output-topic")
-                .short('o')
-                .takes_value(true)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("log-conf")
-                .help("Configure the logging format (example: 'rdkafka=trace')")
-                .long("log-conf")
-                .takes_value(true),
-        )
-        .get_matches();
+    let matches = cli::get_matches();
 
     setup_logger(true, matches.value_of("log-conf"));
 
